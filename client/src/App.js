aae3425028a85cf078184ceffc502fb469648c65
Vishnu-BP <vishnubp71@gmail.com>
2025-10-17 20:45:43 +0530

    Update API_BASE_URL to live Render address

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import {
 
 // --- CONFIGURATION ---
 // Set the base URL for your Express backend
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://portfolio-vishnu-bp.onrender.com';
 
 // --- MOCK DATA (Fallback if API is down) ---
 const mockProjects = [
